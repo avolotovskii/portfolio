d4bdcebe736291b7d6f40d0db5aae11c85c939d3
Anatolii <volotovskiy111@gmail.com>
2022-05-20 08:52:04 +0300

    Добавляет экран с прайс листом и список портфолио

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__scale-percent'),
+    lines = document.querySelectorAll('.skills__scale-progress');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
